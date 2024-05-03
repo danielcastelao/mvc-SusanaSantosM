b753ab91bfb4f8bc3e1ec5168c68e531f05b6d88
SusanaSantosM <ssantosmallqui@danielcastaleo.org>
2024-05-03 11:29:09 +0200

    agregamos el metodo crearCoche

--- src/cod/mvc/Model.java
+++ src/cod/mvc/Model.java
@@ -3,5 +3,11 @@ package cod.mvc;
 import java.util.ArrayList;
 
 public class Model {
-    private ArrayList<Coche> parking = new ArrayList<Coche>();
+    private static ArrayList<Coche> parking = new ArrayList<Coche>();
+
+    public static Coche crearCoche(String matricula, String modelo, Integer velocidad){
+        Coche cocheNuevo = new Coche(matricula, modelo, velocidad);
+        parking.add(cocheNuevo);
+        return cocheNuevo;
+    }
 }
