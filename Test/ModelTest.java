c89a2330df44fe57b1309d9e8f11b13f1917676c
SusanaSantosM <ssantosmallqui@danielcastaleo.org>
2024-05-03 11:56:07 +0200

    Creamos el package Test para probar el programa

---
+++ Test/ModelTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import cod.mvc.Model;
+import cod.mvc.Coche;
+
+public class ModelTest {
+
+    @Test
+    public void pruebaCrearCoche(){
+        Coche coche = Model.crearCoche("501301D","Toyota",0);
+        Assertions.assertEquals("501301D", coche.matricula);
+        Assertions.assertEquals("Toyota", coche.modelo);
+        Assertions.assertEquals(0, coche.velocidad);
+    }
+
+    @Test
+    public void pruebaGetCoche(){
+        Coche coche = Model.crearCoche("501301D","Toyota",0);
+        Assertions.assertSame(coche, Model.getCoche("501301D"));
+    }
+
+    @Test
+    public void
+}
