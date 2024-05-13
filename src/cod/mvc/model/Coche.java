61b0c84d01c3ba8074e04a1ccf18d3ef319f7b2d
SusanaSantosM <ssantosmallqui@danielcastelao.org>
2024-05-13 09:56:09 +0200

    Añadimos comentarios a la clase

--- src/cod/mvc/model/Coche.java
+++ src/cod/mvc/model/Coche.java
@@ -5,9 +5,15 @@ public class Coche {
     public String modelo;
     public Integer velocidad;
 
+    /**
+     * Constructor de la clase Coche
+     * @param matricula es la matricula del coche
+     * @param modelo es el modelo del coche
+     * @param velocidad es la velocidad del coche
+     */
     public Coche(String matricula, String modelo, Integer velocidad){
         this.matricula = matricula;
         this.modelo = modelo;
-        this.velocidad = velocidad;
+        this.velocidad = 0;
     }
 }
