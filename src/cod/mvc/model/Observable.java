f731d9a860b2b26125447c531a5266ca6572abf4
SusanaSantosM <ssantosmallqui@danielcastelao.org>
2024-05-13 09:58:31 +0200

    Comentamos el código

--- src/cod/mvc/model/Observable.java
+++ src/cod/mvc/model/Observable.java
@@ -4,7 +4,20 @@ import cod.mvc.controller.Observer;
 
 public interface Observable {
 
+    /**
+     * Añade un observador a la lista de observadores
+     * @param observer es el observador que se va a añadir
+     */
     void addObserver(Observer observer);
+
+    /**
+     * Elimina un observador de la lista de observadores
+     * @param observer es el observador que se va a eliminar
+     */
     void removeObserver(Observer observer);
-    void notifyObservers();
+
+    /**
+     * Notifica a los observadores
+     */
+    void notifyObservers(Coche cohe);
 }
