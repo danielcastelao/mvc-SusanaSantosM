05da4382e23602c0ee3874a2ff1d12c60e5e7e1c
SusanaSantosM <ssantosmallqui@danielcastelao.org>
2024-05-13 09:58:43 +0200

    Comentamos el código

--- src/cod/mvc/controller/Observer.java
+++ src/cod/mvc/controller/Observer.java
@@ -3,5 +3,10 @@ package cod.mvc.controller;
 import cod.mvc.model.Coche;
 
 public interface Observer {
+
+     /**
+      * Actualiza el estado del coche
+      * @param coche es el coche que se va a actualizar
+      */
      void update(Coche coche);
 }
