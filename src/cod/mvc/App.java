f0959931693be072c276d9f967523461e91b8bd3
SusanaSantosM <ssantosmallqui@danielcastelao.org>
2024-05-24 08:37:06 +0200

    cambiamos la main a App

--- src/cod/mvc/App.java
+++ src/cod/mvc/App.java
@@ -1,7 +1,26 @@
 package cod.mvc;
 
+import cod.mvc.controller.Controller;
+import cod.mvc.model.Model;
+
 public class App {
     public static void main(String[] args){
+        //Inicializamos la aplicación
 
+        //Instanciamos el modelo
+        Model miModel = new Model();
+
+        //Instanciamos el controlador
+        Controller miController = new Controller(miModel);
+
+        //Creamos dos coches
+        miController.crearCoche("1234ABC", "Seat", 100);
+        miController.crearCoche("5678DEF", "Renault", 120);
+
+        //Cambiamos la velocidad de un coche
+        miController.cambiarVelocidad("1234ABC", 150);
+
+        //Otro cambio de velocidad
+        miController.cambiarVelocidad("1234ABC", 250);
     }
 }
