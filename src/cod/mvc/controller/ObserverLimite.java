029eeaa29289b03b554f9d55de6c79e23233bc77
SusanaSantosM <ssantosmallqui@danielcastelao.org>
2024-05-24 10:24:01 +0200

    modificamos el metodo para reducir la velocidad hasta menos del limite

--- src/cod/mvc/controller/ObserverLimite.java
+++ src/cod/mvc/controller/ObserverLimite.java
@@ -1,20 +1,28 @@
 package cod.mvc.controller;
 
 import cod.mvc.model.Coche;
+import cod.mvc.model.Model;
 
 public class ObserverLimite implements Observer{
 
     //Velocidad límite
     int velocidadLimite = 200;
 
+    //Velocidad que reduce al pasar el limite
+    int reduccionDeVelocidad = 10;
+
     /**
      * Metodo que se encarga de actualizar el coche y la nueva velocidad
      * @param coche es el coche que se va a actualizar
      */
-   public void update(Coche coche){
+    @Override
+   public void update(Coche coche, Model model){
       if(coche.velocidad > velocidadLimite){
-          System.out.println("[ObserverLimite] El coche con matricula "+coche.matricula+
-                  " superó el límite de velocidad con "+ coche.velocidad+" km/h");
+          System.out.println("[Observer Limite] INFRACCIÓN: El coche con matricula "+coche.matricula+
+                  " superó el límite de velocidad");
+          model.cambiarVelocidad(coche.matricula, coche.velocidad-reduccionDeVelocidad);
+          System.out.println("Se redujo la velocidad a "+(coche.velocidad-reduccionDeVelocidad)+" km/h");
       }
    }
+
 }
