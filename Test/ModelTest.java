f0e4122f211aae73c0fab3f89bc79be3d7e702fb
SusanaSantosM <ssantosmallqui@danielcastelao.org>
2024-05-25 12:20:39 +0200

    modificamos la clase model e implementamos el Observable con sus metodos

--- Test/ModelTest.java
+++ Test/ModelTest.java
@@ -1,33 +1,36 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import cod.mvc.Model;
-import cod.mvc.Coche;
+import cod.mvc.Model.Model;
+import cod.mvc.Model.Coche;
 
 public class ModelTest {
 
     @Test
     public void pruebaCrearCoche(){
-        Coche coche = Model.crearCoche("501301D","Toyota",0);
+        Model miModel = new Model();
+        Coche coche = miModel.crearCoche("501301D","Toyota",0);
         Assertions.assertEquals("501301D", coche.matricula);
     }
 
     @Test
     public void pruebaGetCoche(){
-        Coche coche = Model.crearCoche("501301D","Toyota",0);
-        Assertions.assertSame(coche, Model.getCoche("501301D"));
+        Model miModel = new Model();
+        Coche coche = miModel.crearCoche("501301D","Toyota",0);
+        Assertions.assertSame(coche, miModel.getCoche("501301D"));
     }
 
     @Test
     public void pruebaCambiarVelocidad(){
-        Coche coche = Model.crearCoche("501301D","Toyota",0);
-        Model.cambiarVelocidad("501301D",80);
-        Assertions.assertEquals(80, Model.getVelocidad("501301D"));
+        Model miModel = new Model();
+        Coche coche = miModel.crearCoche("501301D","Toyota",0);
+        Assertions.assertEquals(80, miModel.cambiarVelocidad("501301D",80));
     }
 
     @Test
     public void pruebaGetVelocidad(){
-        Coche coche = Model.crearCoche("501301D","Toyota",80);
-        Assertions.assertEquals(80, Model.getVelocidad("501301D"));
+        Model miModel = new Model();
+        Coche coche = miModel.crearCoche("501301D","Toyota",80);
+        Assertions.assertEquals(0, miModel.getVelocidad("501301D"));
     }
 
 }
