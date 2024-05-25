dc9d1beb7159315aa48fd09fa287e6793d711d6a
SusanaSantosM <ssantosmallqui@danielcastelao.org>
2024-05-25 12:25:12 +0200

    Colocamos la Main en la App e intanciamos Model y Controller para crear coche y cambiar velocidad

--- src/cod/mvc/App.java
+++ src/cod/mvc/App.java
@@ -1,5 +1,24 @@
 package cod.mvc;
 
+import cod.mvc.Model.Model;
+import cod.mvc.Controller.Controller;
+
 public class App {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+
+        //Instanciamos el modelo
+        Model miModelo = new Model();
+
+        //Instanciamos el controlador
+        Controller miControlador = new Controller(miModelo);
+
+        //Creamos un coche
+        miControlador.crearCoche("129874EU", "Ferrari", 80);
+        miControlador.crearCoche("897435EU", "AudiA3", 100);
+
+        //Cambiamos la velocidad de un coche
+        miControlador.cambiarVelocidad("129874EU", 100);
+        miControlador.cambiarVelocidad("897435EU", 150);
+
+    }
 }
